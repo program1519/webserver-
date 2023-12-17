36d002b73c30e80fb61da243942983fd74de328f
program1519 <109532636+program1519@users.noreply.github.com>
2023-12-17 09:18:22 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+onst http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+  res.setHeader('Content-Type', 'text/html');
+
+
+
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+        } else {
+
+            res.statusCode = 200;
+            res.end(data);
+        }
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
