38dd8e50da94df401bf384a2d3e0d4bbb7426add
program1519 <109532636+program1519@users.noreply.github.com>
2023-12-26 20:02:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,25 +3,12 @@ const fs = require('fs');
 const path = require('path');
 
 const port = 3000;
-
+ const app = express();
+ app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
+    });
 const server = http.createServer((req, res) => {
 
-  res.setHeader('Content-Type', 'text/html');
-
-
-
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-        if (err) {
-            res.statusCode = 500;
-            res.end('Internal Server Error');
-        } else {
-
-            res.statusCode = 200;
-            res.end(data);
-        }
+   server.listen(80, () => {
+      console.log('server run...');
     });
-});
-
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
